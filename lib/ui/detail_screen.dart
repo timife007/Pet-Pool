819440b8760823e5da57f75b4adf2eb176081b86
Timothy Ademola <timife007@gmail.com>
2023-08-06 20:47:23 +0100

    detail screen implemented

--- lib/ui/detail_screen.dart
+++ lib/ui/detail_screen.dart
@@ -8,6 +8,57 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    double containerWidth = MediaQuery.of(context).size.width;
+    double containerHeight = MediaQuery.of(context).size.height / 2;
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0, left: 10, right: 10),
+            child: Stack(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.network(
+                        width: containerWidth,
+                        height: containerHeight,
+                        fit: BoxFit.cover,
+                        petItem.imageLink,
+                      ),
+                    )),
+                Positioned(
+                  top: 5,
+                  left: 5,
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                        color: Colors.white, shape: BoxShape.circle),
+                    child: IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(Icons.arrow_back_ios_new_sharp),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              petItem.name,
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.start,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
